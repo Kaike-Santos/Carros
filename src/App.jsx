5821774d3841de95a273dfbc1c0f0b0147606793
kassike <kaikems27@outlook.com>
2024-03-18 11:08:13 -0300

    ultimo commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ import Luxuosos from "./components/Luxuosos"
 export default function App(){
   return(
     <main className="border-2 border-orange min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justify-center itens-center">
-      <section className="lg:flex max-w-[920px]">
-        
+      <div className="lg:flex max-w-[920px]">
+
         <Sedans/>
 
         <Suvs/>
 
         <Luxuosos/>
         
-      </section>
+      </div>
     </main>
   )
 }
